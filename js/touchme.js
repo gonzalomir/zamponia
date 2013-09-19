55697efe0e62a921ab56f228a108cbcae6d0d6f2
Gonzalo Miranda <gonzalomir@gmail.com>
2013-09-19 10:50:27 -0400

    Ajustes

--- js/touchme.js
+++ js/touchme.js
@@ -1,3 +1,19 @@
-function playD(){
-	alert('hola mundo');
-}
+window.addEventListener('load', function(){
+			var z1 = document.getElementById('zamponia');
+			z1.addEventListener('touchstart', function(e){
+  			var touchobj = e.changedTouches[0];
+  			alert('se toco la pantalla');
+  			e.preventDefault();
+ 			}, false);
+ 
+	 		z1.addEventListener('touchmove', function(e){
+  			alert('hay un desplazamiento en la pantalla touch');
+  			var touchobj = e.changedTouches[0];
+ 			}, false);
+	 
+	 		z1.addEventListener('touchend', function(e){
+  			var touchobj = e.changedTouches[0];
+  			alert('touchend event');
+  			e.preventDefault();
+ 			}, false);
+		}, false);
